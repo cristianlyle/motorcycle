39363c20dfba5048b5eda295ac3b07a47bc1a271
cristianlyle <cristianlyle@gmail.com>
2025-06-13 12:27:22 +0800

    06-13-2025

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,83 @@
-import { StyleSheet, Text, View } from "react-native";
-import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
-
+import CircleButton from "@/components/CircleButton";
+import EmojiList from "@/components/EmojiList";
+import EmojiPicker from "@/components/EmojiPicker";
+import EmojiSticker from "@/components/EmojiSticker";
+import IconButton from "@/components/IconButton";
+import ImageViewer from "@/components/ImageViewer";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
 const PlaceholderImage = require("../../assets/images/background-image.png");
 export default function Index() {
+
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(undefined);
+
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [pickedEmoji, setPickedEmoji] = useState<string | undefined>(undefined);
+
+  const pickIamgeAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+      console.log(result);
+    } else {
+      console.log("Image picking was canceled");
+    }
+  };
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+const onModalClose = () => {
+  setIsModalVisible(false);
+};
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+
+  };
+  const onSaveImageAsync = async () => {
+
+  };
+
   return (
-    <View
-      style={styles.container}
-  > 
-  <View 
-  style={styles.imageContainer}
-  >
-    <ImageViewer imgSource={PlaceholderImage}/>
-
+    <View style={styles.container}> 
+  <View style={styles.imageContainer}>
+    <ImageViewer imgSource={selectedImage || PlaceholderImage} />
+    {pickedEmoji && ( 
+      <EmojiSticker imageSize={40} stickerSource={pickedEmoji}/> )}
          </View>
-         <View>
-          <Button label="Choose a Photo" />
+         {showAppOptions?(
+          <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+          </View>
+        </View>
+        ):(
+         <View style={styles.footerContainer}>
+          <Button 
+          onPress={pickIamgeAsync}
+          label="Choose a Photo" theme="primary" />
+          <Button 
+          label="Delete a Photo"  theme="secondary"
+          onPress={() => setShowAppOptions(true)}
+          />
           
-
          </View>
-    </View>
+)}
+          <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose}/>
+          </EmojiPicker>
+ </View>
   );
 }
 
@@ -39,7 +96,16 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   footerContainer: {
-    width: 1/3,
+    flex: 0.33, 
     alignItems: "center",
+    justifyContent: "center", 
+  },
+  optionsContainer: {
+   position: "absolute",
+   bottom: 80,
+  },
+  optionsRow: {
+    flexDirection: "row",
+   alignItems: "center",
   },
 });
--- components/Button.tsx
+++ components/Button.tsx
@@ -1,22 +1,57 @@
-import { StyleSheet, View, Pressable, Text } from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 type Props = {
   label: string;
+  theme?: 'primary' | 'secondary';
+  onPress?: () => void;
 };
 
-export default function Button({ label }: Props) {
-  return (
-    <View style={styles.buttonContainer}>
-      <Pressable 
-      style={styles.button} 
-      onPress={() => alert('You pressed a button.')}
-      >
-        <Text style={styles.buttonLabel}>{label}</Text>
-      </Pressable>
-    </View>
-  );
-}
+export default function Button({ label, theme, onPress }: Props) {
+  if (theme === 'primary') {
+    return (
+      <View
+        style={[
+          styles.buttonContainer,
+         
+        ]}>
+        <Pressable
+          style={[styles.button, { backgroundColor: '#547792' }]}
+          onPress={onPress}>
+          <FontAwesome name="picture-o" 
+          size={18} color="white" 
+          style={styles.buttonIcon} />
+          
+          <Text style={[styles.buttonLabel, 
+            { color: 'white' }]}>
+              {label}</Text>
+        </Pressable>
+      </View>
+    );
+  }
+  if (theme === 'secondary') {
+    return (
+      <View
+        style={[
+          styles.buttonContainer,
+         
+        ]}>
+        <Pressable
+          style={[styles.button, { backgroundColor: '#547792' }]}
+          onPress={onPress}>
+          <FontAwesome name="trash-o" 
+          size={18} color="white" 
+          style={styles.buttonIcon} />
+          
+          <Text style={[styles.buttonLabel, 
+            { color: 'white' }]}>
+              {label}</Text>
+        </Pressable>
+      </View>
+    );
+  }
 
+}
 const styles = StyleSheet.create({
   buttonContainer: {
     width: 320,
@@ -33,6 +68,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row',
+  },
+  buttonIcon: {
+    paddingRight: 8,
   },
   buttonLabel: {
     color: '#fff',
